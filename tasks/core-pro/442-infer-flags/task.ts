7185ffb7986bb521851129eff5ebeb9c17a6613c
Kacper Kłosowski <kacpermklosowski@gmail.com>
2025-01-07 21:14:51 +0100

    solve exercise 442

--- tasks/core-pro/442-infer-flags/task.ts
+++ tasks/core-pro/442-infer-flags/task.ts
@@ -1,11 +1,27 @@
 import { type FeatureFlags } from './legacy-flags.ts';
 
-type FlagsV2<T> = {};
+type StripV2<T> = T extends `${infer First}V2${infer Second}` ? `${First}${Second}` : never;
 
-export type ModernFeatureFlags = FlagsV2<FeatureFlags>;
+type ModernFeatureFlags = {
+  [Prop in keyof FeatureFlags as StripV2<Prop>]: FeatureFlags[Prop];
+};
 
 export function getFeatureFlagsV2(flags: FeatureFlags): ModernFeatureFlags {
-  const flagsV2: ModernFeatureFlags = {};
+  const modernFlags: ModernFeatureFlags = Object.entries(flags).reduce((acc, [key, value]) => {
+    const match = key.match(/(.+)V2(.+)/);
 
-  return flagsV2;
+    if(!match) {
+      return acc;
+    }
+
+    const [, prefix, suffix] = match;
+    const newKey = `${prefix}${suffix}` as keyof ModernFeatureFlags;
+
+    acc[newKey] = value;
+
+    return acc;
+  }
+  , {} as ModernFeatureFlags);
+
+  return modernFlags;
 }
