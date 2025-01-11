7f86310dc9892fdea27cf6e86d5b688f825d660e
Kacper Kłosowski <kacpermklosowski@gmail.com>
2025-01-11 13:37:52 +0100

    solve exercise 631

--- tasks/core-pro/631-extended-local-window/task.ts
+++ tasks/core-pro/631-extended-local-window/task.ts
@@ -2,6 +2,8 @@
   Zrób to w sposób lokalny, który będzie dostępny tylko w tym module.
 */
 
+declare const window: Window & { storage: { temporaryValue: string } };
+
 window.storage = {
   temporaryValue: 'Am I testing this code or is it testing me?',
 };
