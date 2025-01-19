a2d28beb2c1f11584d0874b8bfa6a36d30604d5c
Kacper Kłosowski <kacpermklosowski@gmail.com>
2025-01-19 13:32:49 +0100

    solve exercise 731

--- tasks/core-pro/731-js-to-ts/product-catalog.js
+++ tasks/core-pro/731-js-to-ts/product-catalog.js
@@ -1,5 +1,17 @@
 import { readFileSync } from 'fs';
 
+/**
+ * @typedef {Object} Product
+ * @property {number} id
+ * @property {string} name
+ * @property {number} price
+ * @property {boolean} [refurbished]
+ */
+
+/**
+ * @description Get the product catalog
+ * @returns {{products: Product[]}}
+ */
 export function getProductCatalog() {
   return JSON.parse(readFileSync(new URL('./catalog.json', import.meta.url), 'utf8'));
 }
