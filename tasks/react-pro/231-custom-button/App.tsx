f1eda379ae60125587cc8ea6df5a04d86844c1e3
Kacper Kłosowski <kacpermklosowski@gmail.com>
2025-01-22 21:30:03 +0100

    (react) solve exercise 231

--- tasks/react-pro/231-custom-button/App.tsx
+++ tasks/react-pro/231-custom-button/App.tsx
@@ -2,8 +2,19 @@
   Zdefiniuj typowanie propsów dla CustomButton, które pozwoli na przekazanie dowolnych atrybutów elementu HTML button.
 */
 
-const CustomButton = ({ children }) => (
-  <button className="p-2 text-white bg-blue-500 rounded-md">{children}</button>
+import {ComponentProps, ReactNode} from 'react';
+
+interface CustomButtonProps extends ComponentProps<'button'> {
+  children: ReactNode;
+}
+
+const CustomButton = ({ children, ...props }: CustomButtonProps) => (
+  <button
+    className="p-2 text-white bg-blue-500 rounded-md"
+    {...props}
+  >
+    {children}
+  </button>
 );
 
 // Przykładowe użycie komponentu Card
